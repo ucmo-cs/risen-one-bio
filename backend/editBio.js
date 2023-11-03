8c72e621ab251292d80dc4f126ab43fee4a6918c
Mason Minihan <msm84470@ucmo.edu>
2023-11-03 15:19:19 -0500

    Adding in baseline backend functions
    Adding in baseline backend functions, I got the serverless.yml file to deploy properly to AWS, the functions themselves are not fully functional yet

---
+++ backend/editBio.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+const uuid = require('uuid');
+
+exports.createBio = async (event, context, callback) => {
+    let headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+    };
+    let statusCode = 200;
+
+    const data = JSON.parse(event.body);
+    console.log("EVENT:::", data);
+
+    //create new timestamp value
+    let d = new Date();
+    let h = addZero(d.getHours());
+    let m = addZero(d.getMinutes());
+    let ts = h + ':' + m;
+    //create new date value
+    let MM = addZero(d.getMonth()+1);
+    let dd = addZero(d.getDate());
+    let y = d.getFullYear();
+    let dt = y + '/' + MM + '/' + dd;
+
+    const params = {
+        TableName: process.env.BIO_TABLE,
+        Item: {
+            id: uuid.v1(),
+            firstName: data.firstName,
+            lastName: data.lastName,
+            jobTitle: data.jobTitle,
+            description: data.description,
+            createdDate: dt,
+            createdTimestamp: ts
+        }
+    }
+
+    console.log("Creating Bio");
+
+    try{
+        await dynamoDb.put(params).promise()
+            .then(res => {
+                callback(null, {
+                    statusCode,
+                    headers,
+                    body: JSON.stringify({message: 'Created Bio Successfully!'})
+                });
+            }).catch(err => {
+                console.log(err);
+                callback(null, {
+                    statusCode: 500,
+                    headers,
+                    body: JSON.stringify({message: 'Unable to Create Bio'})
+                });
+            });
+    } catch (err) {
+        return { error: err }
+    }
+};
+
+
+function addZero(i) {
+    if (i<10) {
+        i = '0' + i;
+    }
+    return i;
+}
