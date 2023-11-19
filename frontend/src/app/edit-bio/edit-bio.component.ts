250a00a73bd72264a7fef1654171a1a58e18242b
Mac42870 <118951692+Mac42870@users.noreply.github.com>
2023-11-19 12:32:13 -0600

    Add files via upload

--- frontend/src/app/edit-bio/edit-bio.component.ts
+++ frontend/src/app/edit-bio/edit-bio.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import {OnInit} from '@angular/core';
 import {Form, FormControl, FormGroup, Validators} from '@angular/forms'
 
@@ -15,9 +14,23 @@ export class EditBioComponent implements OnInit {
   form!: FormGroup;
 
   techStackList: string[] = [];
-
-    
+  isUploaded1: boolean = false;
+  isUploaded2: boolean = false;
+  isUploaded3: boolean = false;
   fileChosen3 = document.getElementById('file-chosen3')!;
+
+//   const input: HTMLInputElement | null = document.getElementById("myInput");
+// if (input) {
+//   input.addEventListener("keypress", function(event: KeyboardEvent) {
+//     if (event.key === "Enter") {
+//       event.preventDefault();
+//       const myBtn: HTMLElement | null = document.getElementById("myBtn");
+//       if (myBtn) {
+//         myBtn.click();
+//       }
+//     }
+//   });
+// }
   ngOnInit() {
     this.form = new FormGroup({
       tech: new FormControl('', Validators.required)
@@ -29,16 +42,26 @@ export class EditBioComponent implements OnInit {
   }
  
   onFileSelectedImage(event: Event) {
+    // const imageElement = document.getElementById("iconImage");
+    // if (imageElement != null){
+    //   // imageElement.setAttribute("src", "../../assets/DeleteImage.png");
+    //   imageElement.setAttribute("ngIf", "true")
+    // }
+   
     const target = event.target as HTMLInputElement;
     if (target.files && target.files.length > 0) {
+      
       if(target.id === "upload-img-btn1"){
         document.getElementById('file-chosen1')!.textContent = target.files[0].name
+        this.isUploaded1 = true;
       }
       else if(target.id === "upload-img-btn2"){
         document.getElementById('file-chosen2')!.textContent = target.files[0].name
+        this.isUploaded2 = true;
       }
       else if (target.id === "upload-img-btn3"){
         document.getElementById('file-chosen3')!.textContent = target.files[0].name
+        this.isUploaded3 = true;
       }
         console.log(target.files[0].name);
         console.log(target.id);
